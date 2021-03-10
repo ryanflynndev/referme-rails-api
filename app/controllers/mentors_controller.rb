6b5fa24023909cc51b455c78e13da0780d2dbd4b
Ryan Flynn <ryanflynn44@gmail.com>
2021-03-09 21:35:57 -0500

    Added create method and params for mentor

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,2 +1,11 @@
 class MentorsController < ApplicationController
+    def create
+        mentor = Mentor.create(mentor_params)
+    end
+
+
+    private
+
+    def mentor_params
+        params.require(:mentor).permit(:username, :email, :company_name)
 end
